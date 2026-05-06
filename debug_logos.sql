e0b53b79e6c7b33fd9a8a30376e9282e1062250e
alexanderchernavin-cloud <alexander.chernavin@oddin.gg>
2026-05-06 11:03:45 +0200

    chore: drop accidentally-committed debug SQL files (#118)

--- debug_logos.sql
+++
@@ -1,6 +0,0 @@
-SELECT s.slug, c.id, c.name, c.logo_url
-FROM competitors c
-JOIN sports s ON s.id = c.sport_id
-WHERE c.active = true
-  AND c.logo_url LIKE '/team-logos/%'
-ORDER BY s.slug, c.name;
